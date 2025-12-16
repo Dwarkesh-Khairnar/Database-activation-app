19d83fe0bbe20de647f08a1742f2e74c36b8caa0
Dwarkesh-Khairnar <dwarkeshkhairnar12@gmail.com>
2025-12-16 22:03:28 +0530

    add api update code

---
+++ api/update.js
@@ -0,0 +1,26 @@
+import { connectDatabase, client } from "../db/dbConnection.js";
+
+export default async function handler(req, res) {
+  try {
+    await connectDatabase();
+
+    // Check time
+    const hour = new Date().getHours();
+    const statusText = hour >= 12 ? "Evening" : "Morning";
+
+    console.log("CRON TRIGGERED AT:", new Date().toISOString());
+
+    const result = await client.query(
+      "UPDATE active SET active=$1 WHERE id=$2",
+      [statusText, 1]
+    );
+
+    return res.status(200).json({
+      message: `Updated to ${statusText}`,
+      result,
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return res.status(500).json({ error: error.message });
+  }
+}
